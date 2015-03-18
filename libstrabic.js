656a2f04106fc11a8524e876c04e5f757327873c
Loann Neveu <neveu.loann@gmail.com>
2015-03-18 23:41:53 +0100

    Change style + add output folder

--- libstrabic.js
+++ libstrabic.js
@@ -6,8 +6,8 @@ window.onload = function () {
 		$(this).parent().parent().css('zIndex','100');
 	});
 	$( "img" ).bind( "mouseout", function() {
-		this.style.width = 60;
-		this.style.height = 60;
+		this.style.width = 80;
+		this.style.height = 80;
 		$(this).parent().parent().css('zIndex',100);
 		$(this).parent().parent().find(".author, .title").show();
 	});
